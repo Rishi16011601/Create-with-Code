0a9c02f7561b36142fe31a1c84c56f1bac3d6a6e
Rishiwar Asija <rishiwarasija@Rishiwars-MacBook-Pro.local>
2021-05-02 13:41:20 +0530

    P2 commit

---
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    private float lowBound = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Out of boundary destroys object
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowBound)
+        {
+            //Game over Message when out Boundary
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype 2/Assets/Scripts/SpwanManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpwanManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spwanRangeX = 20;
+    private float spwanPosZ = 20;
+    private float startDelay = 2;
+    private float spwanInterval = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spwanInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void SpawnRandomAnimal()
+    {
+        //Random postion
+        Vector3 spawnPos = new Vector3(Random.Range(-spwanRangeX, spwanRangeX), 0, spwanPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        //Cloning of Animal Prefabs
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
